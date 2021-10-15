0056e371a33a22238a62137a618340acddf15fcc
income3000 <income3000@gmail.com>
2021-10-15 04:52:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height='100vh'
+      projectID='
+c4617c1b-b106-4f65-bcff-5651a09ccd2a'
+      userName='income3000'
+      userSecret='123123'
+      renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
+    />
   );
 }
 
